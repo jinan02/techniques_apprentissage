a6a21574919f94658fd8cef32ef48dbe764e474d
Jinane Boufaris <102619697+jinan02@users.noreply.github.com>
2023-10-03 07:41:58 +0100

    Update solution_regression.py

--- solution_regression.py
+++ solution_regression.py
@@ -6,8 +6,9 @@
 
 import numpy as np
 import random
-import gestion_donnees
 from sklearn import linear_model
+from sklearn.utils import shuffle
+
 
 
 class Regression:
@@ -24,18 +25,40 @@ class Regression:
 
         NOTE : En mettant phi_x = x, on a une fonction de base lineaire qui fonctionne pour une regression lineaire
         """
-        phi_x = np.array([])
         # AJOUTER CODE ICI
+        
+        '''phi_x = []
+
         if isinstance(x, (float,int)):
             # Si x est un scalaire
-            phi_x = np.array([x**i for i in range(1, self.M + 1)])
-        elif (isinstance(x, np.ndarray) and x.ndim==1 ):
+            phi_x = [x**i for i in range(1, self.M + 1)]
+        elif isinstance(x, np.ndarray):
             # Si x est une liste de scalaires
             phi_x = np.array([[y**i for i in range(1, self.M + 1)] for y in x])
-        # phi_x = x
-        return phi_x
+        return (phi_x)'''
+        if(type(x) == np.ndarray):  #Si x est un vecteur de N scalaires, alors phi_x sera un tableau 2D de taille NxM
+            phi_x = []
+            for i in x:
+                phi_x += [[i**j for j in range(1, self.M + 1)]]
+            phi_x = np.array(phi_x)
+        else:                       #Si x est un scalaire, alors phi_x sera un vecteur à self.M dimensions : (x^1,x^2,...,x^self.M)
+            phi_x = np.array([x**i for i in range(1, self.M + 1)])
 
-    def recherche_hyperparametre(self, X, t,k=10):
+        return phi_x
+    
+        '''if x.ndim == 0:
+            phi_x = [x**i for i in range(1, self.M + 1)]
+            # phi_x = x ** np.arange(1, self.M+1 )
+        else:
+            # phi_x = np.empty([np.shape(x)[0], self.M])
+            phi_x = np.ones([np.shape(x)[0], self.M])
+            phi_x = [[y**i for i in range(1, self.M + 1)] for y in x]
+            phi_x=np.array(phi_x)'''
+
+        return phi_x
+    
+
+    def recherche_hyperparametre(self, X, t):
         """
         Trouver la meilleure valeur pour l'hyper-parametre self.M (pour un lambda fixe donné en entrée).
 
@@ -44,67 +67,57 @@ class Regression:
         pour diviser les données en "k" parties. Si le nombre de données en entrée N est plus petit que "k", 
         k devient égal à N. Il est important de mélanger les données ("shuffle") avant de les sous-diviser
         en "k" parties."""
+
+        k=10
+
+        '''data = np.concatenate((X,t.T))
+        random.shuffle(data)'''
+
+        if k > len(X): 
+            k = len(X)
         
+        X_shuffled, t_shufled = shuffle(X, t, random_state=0)
+        X_folds = np.array_split(X_shuffled, k)
+        t_folds = np.array_split(t_shufled, k)
+        
+        '''data_folds = np.array_split(data,k)
+        data_folds = np.array(data_folds)
+        print(data_folds.shape)'''
+        average_errors = {}
+        m_values = [i for i in range(1, 11)]
+        min_value = float('inf')
+        self.M = 1
 
-        indices_shuffled = list(range(len(t)))
-        random.shuffle(indices_shuffled)
-        X = X[indices_shuffled]
-        t = t[indices_shuffled]
+        for m in m_values : 
 
-        if k>len(t) :
-            k = len(t)
-
-        X_folds = np.array_split(X,k)
-        t_folds = np.array_split(t,k)
-
-        m_values = [1, 2, 3, 4, 5]
-        avg_scores = []
-        best_m = None
-        best_score = float('inf')
-
-        for m in m_values :
-
-            model = linear_model.LinearRegression()
-            scores = []
-            for i in range(k):
-
-                X_train = np.concatenate([X_folds[j] for j in range(k) if j != i])
+            all_mse = []
+            for i in range(k): 
+                
+                X_train = np.delete(X_folds,i)
                 X_valid = X_folds[i]
 
-                t_train = np.concatenate([t_folds[j] for j in range(k) if j != i])
+                t_train = np.delete(t_folds,i)
                 t_valid = t_folds[i]
+              
 
-                X_train_poly = np.power(X_train, m)
-                X_test_poly = np.power(X_valid, m)
+                self.entrainement(X_train, t_train, False)
 
-                X_train_poly = X_train_poly.reshape(-1, 1)
+                predictions = np.array([self.prediction(x_valid) for x_valid in X_valid])
 
-                model.fit(X_train_poly, t_valid)
+                MSE = np.mean(self.erreur(t_valid, predictions))
+                all_mse.append(MSE)
 
-                t_pred = model.predict(X_test_poly)
+            average_errors[m] = np.mean(all_mse)
 
-                metric = np.mean((t_valid - t_pred) ** 2)
-
-                scores.append(metric)
-            
-            avg_scores.append(np.mean(scores))
-
-            if np.mean(scores) < best_score:
-                best_m = m
-                best_score = np.mean(scores)
+        for key, value in average_errors.items():
+            if value < min_value: 
+                min_value  = value
+                self.M = key
         
-  
-        
-        return best_m
+        return self.M
 
-
-
-
-
-
-
-
-
+    
+    
         """
         Option 2
         Sous-échantillonage aléatoire avec ratio 80:20 pour Dtrain et Dvalid, avec un nombre de répétition k=10.
@@ -113,14 +126,12 @@ class Regression:
 
         Le resultat est mis dans la variable self.M
         X: vecteur de donnees
-        t: vecteur de cibles
-        """
-        # AJOUTER CODE ICI
-        self.M = 1
+        t: vecteur de cibles """
+        
 
     def entrainement(self, X, t, using_sklearn=False):
-        """
-        Entraîne la regression lineaire sur l'ensemble d'entraînement forme des
+        
+        """Entraîne la regression lineaire sur l'ensemble d'entraînement forme des
         entrees ``X`` (un tableau 2D Numpy, ou la n-ieme rangee correspond à l'entree
         x_n) et des cibles ``t`` (un tableau 1D Numpy ou le
         n-ieme element correspond à la cible t_n). L'entraînement doit
@@ -146,10 +157,26 @@ class Regression:
         """
         #AJOUTER CODE ICI
         if self.M <= 0:
-            self.recherche_hyperparametre(X, t)
+           self.recherche_hyperparametre(X, t)
 
         phi_x = self.fonction_base_polynomiale(X)
-        self.w = [0, 1]
+        
+
+        if using_sklearn == True:
+            reg = linear_model.Ridge(alpha = self.lamb)
+            reg.fit(phi_x, t)
+            self.w = reg.coef_
+            self.w[0] = reg.intercept_
+        
+        else:
+            I = np.identity(phi_x.shape[1])
+            produit_phi = np.dot(np.transpose(phi_x),phi_x)
+            t = np.array(t)
+            self.w = np.linalg.solve(self.lamb * I + produit_phi, np.transpose(phi_x).dot(t) )
+
+        #return self.w
+
+        
 
     def prediction(self, x):
         """
@@ -161,7 +188,9 @@ class Regression:
         afin de calculer la prediction y(x,w) (equation 3.1 et 3.3).
         """
         # AJOUTER CODE ICI
-        return 0.5
+
+        phi_x = self.fonction_base_polynomiale(x)
+        return np.dot(np.transpose(self.w), phi_x)
 
     @staticmethod
     def erreur(t, prediction):
@@ -170,14 +199,7 @@ class Regression:
         la cible ``t`` et la prediction ``prediction``.
         """
         # AJOUTER CODE ICI
-        return 0.0
+        return (prediction - t) ** 2
 
 
-    
 
-test = Regression(5,5)
-data = gestion_donnees.GestionDonnees([1,2,3,4,5],"linear",50,10,2)
-
-x_train, t_train, x_test, t_test = data.generer_donnees()
-result = test.recherche_hyperparametre(x_train,t_train)
-print(result)
